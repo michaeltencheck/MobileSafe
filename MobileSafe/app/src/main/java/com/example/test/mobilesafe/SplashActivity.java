19ad1d2bd3d76e7430ceacbea7c43453b20e39ae
michael <neo21anderson@gmail.com>
2015-10-26 15:10:02 +0800

    implement fullscreen for splash activity
    implement fullscreen for splash activity

--- MobileSafe/app/src/main/java/com/example/test/mobilesafe/SplashActivity.java
+++ MobileSafe/app/src/main/java/com/example/test/mobilesafe/SplashActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     @Override
