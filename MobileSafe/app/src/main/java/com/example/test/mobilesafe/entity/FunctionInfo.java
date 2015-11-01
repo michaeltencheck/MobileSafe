003e122ffba5ef96d001a53419ca7a044a2c0536
michael <neo21anderson@gmail.com>
2015-11-01 19:34:07 +0800

    create entity class of function info
    create entity class of function info

---
+++ MobileSafe/app/src/main/java/com/example/test/mobilesafe/entity/FunctionInfo.java
@@ -0,0 +1,27 @@
+package com.example.test.mobilesafe.entity;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by test on 11/1/2015.
+ */
+public class FunctionInfo {
+    private Drawable drawable;
+    private String name;
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
