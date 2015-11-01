f3ac60714e9ae8d88c114c17fa90aefe4cf7cdc9
michael <neo21anderson@gmail.com>
2015-11-01 20:03:03 +0800

    create an adapter to show function
    create an adapter to show function

--- MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/MainActivity.java
+++ MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.test.mobilesafe.activity;
 
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.test.mobilesafe.R;
-import com.example.test.mobilesafe.utility.Downloader;
-import com.example.test.mobilesafe.utility.Logger;
-import com.example.test.mobilesafe.utility.SDCardChecker;
-
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
