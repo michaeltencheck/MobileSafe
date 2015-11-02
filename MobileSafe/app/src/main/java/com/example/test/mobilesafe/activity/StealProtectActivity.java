9b00cbc41449b966c6a2176039387be84e7d74c1
michael <neo21anderson@gmail.com>
2015-11-02 20:20:35 +0800

    create a dialog for inputting password
    create a dialog for inputting password

--- MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/StealProtectActivity.java
+++ MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/StealProtectActivity.java
@@ -1,5 +1,7 @@
 package com.example.test.mobilesafe.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,6 +20,8 @@ public class StealProtectActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getAlertDialog();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +30,27 @@ public class StealProtectActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void getAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.password);
+        View view = View.inflate(this, R.layout.dialog_content_edittext, null);
+        builder.setView(view);
+        builder.setCancelable(false);
+        builder.setPositiveButton(R.string.comfirm, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
     }
 
 }
