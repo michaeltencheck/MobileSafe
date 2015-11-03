cfcf03d7e9fa0ba35c59a22ed6da7b2db2bb9eb7
michael <neo21anderson@gmail.com>
2015-11-03 20:47:49 +0800

    complete password setting function
    complete password setting function and add animation for it

--- MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/StealProtectActivity.java
+++ MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/StealProtectActivity.java
@@ -9,6 +9,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.test.mobilesafe.R;
 import com.example.test.mobilesafe.utility.Logger;
@@ -16,6 +20,7 @@ import com.example.test.mobilesafe.utility.Logger;
 public class StealProtectActivity extends AppCompatActivity {
     private static final String TAG = "StealProtectActivity";
     private SharedPreferences.Editor editor;
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,11 @@ public class StealProtectActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        animation = AnimationUtils.loadAnimation(this, R.anim.shake);
+
         editor = getSharedPreferences("config", MODE_PRIVATE).edit();
+
+        whetherPasswordSet();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,22 +45,25 @@ public class StealProtectActivity extends AppCompatActivity {
         });
     }
 
-    private boolean isPasswordSet() {
+    private void whetherPasswordSet() {
         String pwd = getSharedPreferences("config", MODE_PRIVATE).getString("password", "");
         if (pwd.equals("")) {
             Logger.i(TAG, "password is " + pwd);
-            return false;
+            setPwdDialog();
         } else {
-            return true;
+            Logger.i(TAG, "password is " + pwd);
+            getAlertDialog();
         }
     }
 
     private void setPwdDialog() {
-        editor.putString("password", "");
-        editor.commit();
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.set_password);
-        View view = View.inflate(this, R.layout.dialog_content_edittext, null);
+
+        View view = View.inflate(this, R.layout.password_setting, null);
+        final EditText setPwd = (EditText) view.findViewById(R.id.set_password);
+        final EditText comfirmPwd = (EditText) view.findViewById(R.id.reset_password);
+
         builder.setView(view);
         builder.setCancelable(false);
         builder.setPositiveButton(R.string.comfirm, new DialogInterface.OnClickListener() {
@@ -66,7 +78,32 @@ public class StealProtectActivity extends AppCompatActivity {
 
             }
         });
-        builder.create().show();
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password1 = setPwd.getText().toString().trim();
+                String password2 = comfirmPwd.getText().toString().trim();
+                /*if (password1.equals(password2) & !(password1.isEmpty())) {
+                    editor.putString("password", password1).commit();
+                    dialog.dismiss();
+                } else {
+                    comfirmPwd.startAnimation(animation);
+                }*/
+                if (password1.isEmpty()) {
+                    comfirmPwd.startAnimation(animation);
+                    Toast.makeText(getApplicationContext(),R.string.empty_pwd,Toast.LENGTH_SHORT).show();
+                }else if (!password1.equals(password2)) {
+                    comfirmPwd.startAnimation(animation);
+                    Toast.makeText(getApplicationContext(), R.string.different_pwd, Toast.LENGTH_SHORT).show();
+                } else {
+                    editor.putString("password", password1).commit();
+                    dialog.dismiss();
+                    Toast.makeText(getApplicationContext(), R.string.pwd_setting_success, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void getAlertDialog() {
