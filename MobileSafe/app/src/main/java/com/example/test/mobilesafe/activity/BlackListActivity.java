b12efb5aa91a98100f2ec9602b7ce6d7547639ec
michael <neo21anderson@gmail.com>
2016-04-07 19:55:35 +0800

    170
    170

--- MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/BlackListActivity.java
+++ MobileSafe/app/src/main/java/com/example/test/mobilesafe/activity/BlackListActivity.java
@@ -1,8 +1,11 @@
 package com.example.test.mobilesafe.activity;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,9 +15,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.test.mobilesafe.R;
@@ -23,6 +28,8 @@ import com.example.test.mobilesafe.entity.BlackList;
 import com.example.test.mobilesafe.entity.FunctionInfo;
 import com.example.test.mobilesafe.utility.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -114,7 +121,13 @@ public class BlackListActivity extends AppCompatActivity {
                 }
                 break;
         }*/
-        retrieveContactNumber();
+        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
+            Log.d(TAG, "Response: " + data.toString());
+            uriContact = data.getData();
+
+            retrieveContactNumber();
+
+        }
     }
 
     private void retrieveContactNumber() {
@@ -155,4 +168,28 @@ public class BlackListActivity extends AppCompatActivity {
 
         Logger.d(TAG, "Contact Phone Number: " + contactNumber);
     }
+
+
+   /* private void retrieveContactPhoto() {
+
+        Bitmap photo = null;
+
+        try {
+            InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(getContentResolver(),
+                    ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, new Long(contactID)));
+
+            if (inputStream != null) {
+                photo = BitmapFactory.decodeStream(inputStream);
+                ImageView imageView = (ImageView) findViewById(R.id.img_contact);
+                imageView.setImageBitmap(photo);
+            }
+
+            assert inputStream != null;
+            inputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }*/
 }
