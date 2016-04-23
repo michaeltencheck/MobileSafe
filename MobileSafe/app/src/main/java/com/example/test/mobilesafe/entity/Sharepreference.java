0d9bb495549eba01c901bbdc8bcd6dd6e2de0363
michael <neo21anderson@gmail.com>
2016-04-23 14:05:40 +0800

    188
    188

--- MobileSafe/app/src/main/java/com/example/test/mobilesafe/entity/Sharepreference.java
+++ MobileSafe/app/src/main/java/com/example/test/mobilesafe/entity/Sharepreference.java
@@ -4,5 +4,9 @@ package com.example.test.mobilesafe.entity;
  * Created by test on 4/22/2016.
  */
 public class Sharepreference {
+    public static final String TITLE = "BLACKLIST";
 
+    public Sharepreference() {
+        super();
+    }
 }
